b7a98086174d39d805bd8dce11d8e692945067d7
datax911 <59479286+datax911@users.noreply.github.com>
2020-02-15 11:49:05 -0500

    Ajoutez des fichiers projet.

---
+++ ChartGUI/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ChartGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(224, 80);
+            this.chart1.Name = "chart1";
+            series1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            series1.ChartArea = "ChartArea1";
+            series1.IsVisibleInLegend = false;
+            series1.LabelForeColor = System.Drawing.Color.Maroon;
+            series1.Legend = "Legend1";
+            series1.Name = "Salary";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(392, 280);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ ChartGUI/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChartGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            fillChart();
+        }
+
+
+
+
+
+
+        private void fillChart()
+        {
+            chart1.Series["Salary"].Points.AddXY("Vince", "50000");
+            chart1.Series["Salary"].Points.AddXY("Paul", "45000");
+            chart1.Series["Salary"].Points.AddXY("Pierre", "28000");
+            chart1.Series["Salary"].Points.AddXY("Phil", "65000");
+
+            chart1.Series["Salary"].Color = Color.PaleVioletRed;
+            chart1.ChartAreas[0].AxisY.Maximum = 100000;
+            chart1.ChartAreas[0].AxisY.Minimum = 10000;
+
+            chart1.Series.Add("test");
+            chart1.Series["test"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["test"].Points.AddXY("Bloops", "67000");
+            chart1.Series["test"].Points.AddXY("sadf", "62000");
+            chart1.Series["test"].Points.AddXY("xzcv", "63000");
+
+
+
+            chart1.Titles.Add("Test1");
+        }
+
+
+
+
+    }
+}
